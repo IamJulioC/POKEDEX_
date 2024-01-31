d35053207862b9a4c4659e39b429699fa3d70788
Julio <iamjulioc@live.com>
2024-01-30 23:59:39 -0300

    Pokedex V.1.4

--- assets/js/pokeApi.js
+++ assets/js/pokeApi.js
@@ -1,18 +1,35 @@
 
 const pokeApi = {}
-pokeApi.getPokemonDetail = (pokemon) => {
-   return fetch(pokemon.url)
-                .then((response) => response.json())
+
+function convertPokeApiDetailToPokemon (pokemonDetails) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokemonDetails.order
+    pokemon.name = pokemonDetails.name
+    
+    const types = pokemonDetails.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    pokemon.types = types 
+    pokemon.type = type
+    
+    pokemon.photo =pokemonDetails.sprites.other.dream_world.front_default
+
+    return pokemon
 }
 
-pokeApi.getPokemons = ( offset = 0, limit = 12) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+   return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
      return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-            .then((detailRequests) => Promise.all(detailRequests))
-            .then((pokemonsDetails) => pokemonsDetails)
-            .catch((error) => console.error(error))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+        .catch((error) => console.error(error))
 }
